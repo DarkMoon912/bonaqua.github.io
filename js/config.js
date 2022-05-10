bb13ea7c559e7bdabb1c97d305a9e70693215e0e
DarkMoon912 <86262929+DarkMoon912@users.noreply.github.com>
2022-05-10 21:54:05 +0700

    bonaqua xyi

---
+++ js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 2.5, 
+
+  // Заголовок
+  title: 'BonaQua#5586',
+
+  // Социальные сети
+  social: {
+    telegram: 'zaynehouse',
+    steam: 'zaynehouse',
+    vk: 'zaynehouse', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#BA55D3',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#BA55D3',
+  // Цвет тени кнопки play
+  playButtonShadowColor: '#0c0b0c)',
+
+
+  // Цвет заголовка
+  titleColor: '#BA55D3',
+  // Цвет тени загаловка
+  titleShadowColor: '#0c0b0c',
+
+
+  // Текст копирайта
+  copyrightText: 'dev - DarKi#1288',
+  // Цвет копирайта
+  copyrightTextColor: '#BA55D3',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#BA55D3',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: '#0c0b0c',
+}
